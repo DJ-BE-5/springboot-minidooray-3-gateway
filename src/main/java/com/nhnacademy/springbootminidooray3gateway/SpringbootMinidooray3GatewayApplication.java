4421a801723ba089d9e7def80215cc45c1789803
김승규 <rlatmdrb1996@gmail.com>
2024-03-08 14:40:23 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/springbootminidooray3gateway/SpringbootMinidooray3GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootminidooray3gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMinidooray3GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMinidooray3GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/springbootminidooray3gateway/SpringbootMinidooray3GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootminidooray3gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMinidooray3GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
