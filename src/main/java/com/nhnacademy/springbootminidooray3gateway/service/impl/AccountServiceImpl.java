3620123b58b59eb5161c02be80ddba06f035ea09
김승규 <rlatmdrb1996@gmail.com>
2024-03-08 20:13:10 +0900

    remove: 레디스 관련 필요없는 설정 삭제

--- src/main/java/com/nhnacademy/springbootminidooray3gateway/service/impl/AccountServiceImpl.java
+++ src/main/java/com/nhnacademy/springbootminidooray3gateway/service/impl/AccountServiceImpl.java
@@ -5,13 +5,11 @@ import com.nhnacademy.springbootminidooray3gateway.domain.Member;
 import com.nhnacademy.springbootminidooray3gateway.dto.request.LoginRequest;
 import com.nhnacademy.springbootminidooray3gateway.service.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
-    private final RedisTemplate<String, Object> redisTemplate;
     private final AccountAdaptor accountAdaptor;
 
     @Override
